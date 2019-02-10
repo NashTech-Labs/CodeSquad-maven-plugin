0c5f590661aba59b2628d7c93680022675260539
randhir <randhir.kumar@knoldus.in>
2019-02-10 21:13:20 +0530

    implemented multi module report upload feature

--- src/main/java/com/knoldus/CodeSquad.java
+++ src/main/java/com/knoldus/CodeSquad.java
@@ -8,42 +8,46 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Map;
 
 @Mojo(name = "ReportUpload", threadSafe = true)
 public class CodeSquad extends AbstractMojo {
 
-    @Parameter(property = "organisationName", required = true)
+    @Parameter(property = "ReportUpload.organisationName", required = true)
     public String organisationName;
-    @Parameter(property = "projectName", required = true)
+    @Parameter(property = "ReportUpload.projectName", required = true)
     public String projectName;
-    @Parameter(property = "moduleName", required = true)
-    public String moduleName;
-    @Parameter(property = "file", required = true)
-    public List<String> files;
-
-    @Parameter(property = "registrationKey", required = true)
+    @Parameter(property = "ReportUpload.registrationKey", required = true)
     public String registrationKey;
 
-    private String route = "http://18.221.78.85:8080/add/reports";
+    @Parameter(property = "ReportUpload.modules", required = true)
+    public Map<String, Module> modules;
+
+
+    private String route = "http://52.15.45.40:8080/add/reports";
 
     public void execute() throws MojoExecutionException {
         try {
-            for (String file : files) {
-                String[] command = {"curl", "-X", "PUT", "-F", "projectName=" + projectName, "-F", "registrationKey=" + registrationKey, "-F",
-                        "moduleName=" + moduleName, "-F", "organisation=" + organisationName, "-F",
-                        "file=@" + file, route};
+            for (Map.Entry<String, Module> reportEntry : modules.entrySet()) {
+                String moduleName = reportEntry.getValue().moduleName;
+                List<String> files = reportEntry.getValue().files;
+                for (String file : files) {
+                    String[] command = {"curl", "-X", "PUT", "-F", "projectName=" + projectName, "-F", "registrationKey=" + registrationKey, "-F",
+                            "moduleName=" + moduleName, "-F", "organisation=" + organisationName, "-F",
+                            "file=@" + file, route};
 
-                ProcessBuilder process = new ProcessBuilder(command);
-                Process p = process.start();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                StringBuilder builder = new StringBuilder();
-                String line = null;
-                while ((line = reader.readLine()) != null) {
-                    builder.append(line);
-                    builder.append(System.getProperty("line.separator"));
+                    ProcessBuilder process = new ProcessBuilder(command);
+                    Process p = process.start();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                    StringBuilder builder = new StringBuilder();
+                    String line = null;
+                    while ((line = reader.readLine()) != null) {
+                        builder.append(line);
+                        builder.append(System.getProperty("line.separator"));
+                    }
+                    String result = builder.toString();
+                    System.out.print(result);
                 }
-                String result = builder.toString();
-                System.out.print(result);
             }
 
         } catch (IOException e) {
---
+++ src/main/java/com/knoldus/Module.java
@@ -0,0 +1,8 @@
+package com.knoldus;
+
+import java.util.List;
+
+public class Module {
+    public String moduleName;
+    public List<String> files;
+}
